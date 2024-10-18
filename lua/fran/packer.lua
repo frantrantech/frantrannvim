10527eec965c1449f6bfb988749410f1da9e2b08
frantrantech <38846461+frantrantech@users.noreply.github.com>
2024-10-17 20:43:30 -0500

    Update packer.lua

--- lua/fran/packer.lua
+++ lua/fran/packer.lua
@@ -9,43 +9,65 @@ return require('packer').startup(function(use)
 
 
   use {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
--- or                            , branch = '0.1.x',
-  requires = { {'nvim-lua/plenary.nvim'} }
-}
-use('nvim-treesitter/nvim-treesitter', {run =  ':TSUpdate'})
-use('nvim-treesitter/nvim-treesitter')
-use('nvim-treesitter/playground')
-use('ThePrimeagen/harpoon')
-use('mbbill/undotree')
-use('tpope/vim-fugitive')
-use { "scottmckendry/cyberdream.nvim" }
-use({'VonHeikemen/lsp-zero.nvim', branch = 'v4.x'})
-use({'neovim/nvim-lspconfig'})
-use({'hrsh7th/nvim-cmp'})
-use({'hrsh7th/cmp-nvim-lsp'})
-use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    -- or                            , branch = '0.1.x',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+  use('nvim-treesitter/nvim-treesitter')
+  use('nvim-treesitter/playground')
+  use('ThePrimeagen/harpoon')
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
+  use { "scottmckendry/cyberdream.nvim" }
+  use({ 'neovim/nvim-lspconfig' })
+  use({ 'hrsh7th/nvim-cmp' })
+  use({ 'hrsh7th/cmp-nvim-lsp' })
+  use {
     "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
-}
-use('ThePrimeagen/vim-be-good')
-use('Mofiqul/vscode.nvim')
-use {
+  }
+  use('ThePrimeagen/vim-be-good')
+  use('Mofiqul/vscode.nvim')
+  use {
     'numToStr/Comment.nvim',
     config = function()
-        require('Comment').setup()
+      require('Comment').setup()
     end
-}
+  }
 
-use {
+  use {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
     config = function()
-        require("nvim-autopairs").setup {}
+      require("nvim-autopairs").setup {}
     end
-}
+  }
 
-use 'rebelot/kanagawa.nvim'
+  use {
+    "pmizio/typescript-tools.nvim",
+    requires = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    config = function()
+      require("typescript-tools").setup {}
+    end,
+  }
 
+  use('jose-elias-alvarez/null-ls.nvim')
+  use('MunifTanjim/prettier.nvim')
+
+  use('rose-pine/neovim')
+  use {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  }
+  use "rebelot/kanagawa.nvim"
+
+
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+  }
 
 end)

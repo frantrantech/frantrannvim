47e4a135ea03fb3c51b51bca4dad35f9a3edc783
frantrantech <francuhz4@gmail.com>
2025-04-05 14:34:58 -0500

    Added functionality to quit file if no changes to buffer

--- lua/fran/remap.lua
+++ lua/fran/remap.lua
@@ -48,10 +48,12 @@
 ------------- Map Keys Here --------------
 
 vim.g.mapleader = " "
+
 -- Move to virtual lines instead of real lines -- 
 vim.keymap.set('n', 'j', 'gj', { noremap = true })
 vim.keymap.set('n', 'k', 'gk', { noremap = true })
 
+-- Go to netrw --
 vim.keymap.set("n", "<leader>lv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -60,20 +62,26 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Going to next match centers screen --
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+-- Paste and send pasted over to black hole register --
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
+-- Yank to clipboard register --
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
+-- Delete to black hole register --
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
+-- Ctrl j, Ctrl k cycle quickfix list
 vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
 
--- close quick fix with leader qf
+-- close quick fix with leader qf or ff
 vim.keymap.set('n', '<leader>qf', ':cclose<CR>')
 vim.keymap.set('n', '<leader>ff', ':cclose<CR>')
 
@@ -97,15 +105,17 @@ vim.keymap.set('n', '<leader>i', ':vsplit<cr><C-w>w<leader>1', { remap = true })
 
 -- Save File
 vim.keymap.set('n', '<leader>;', ':w<cr>')
+
 -- Save and quit buffer. If in netrw, just quit
 vim.keymap.set("n", "<leader>n", function()
   if vim.bo.filetype == "netrw" then
-    vim.cmd("q")  -- Quit without saving if inside netrw
+    vim.cmd("q")  -- Quit netrw normally
+  elseif vim.bo.modified then
+    vim.cmd("wq") -- Save and quit if buffer has unsaved changes
   else
-    vim.cmd("wq") -- Save and quit otherwise (in a buffer)
+    vim.cmd("q!") -- Quit without saving if no changes
   end
 end, { noremap = true, silent = true })
-
 -- remaps to find matches revisit later if layer keys are bad xd
 -- vim.keymap.set('n', '<leader>m', "%")
 -- vim.keymap.set('n', '<leader>;', "*")
